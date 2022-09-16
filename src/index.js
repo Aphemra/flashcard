c87eb390489941633aedc7887e2fcfa1272e4add
aphemra <aphemra@gmail.com>
2022-09-16 19:07:38 -0400

    Deployment bug fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Layout from "./pages/Layout";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/flashcard/"}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<App />} />
